8335dede5f4c628e519954b39d0c54b22e3e973e
crystxlised <avkxdlol@gmail.com>
2026-09-25 16:28:23 +0530

    Remove remaining repository files

--- app/src/main/java/com/example/data/ScoreRepository.kt
+++
@@ -1,9 +0,0 @@
-package com.example.data
-
-import kotlinx.coroutines.flow.Flow
-
-class ScoreRepository(private val scoreDao: ScoreDao) {
-    val topScores: Flow<List<ScoreEntity>> = scoreDao.getTopScores()
-    suspend fun insertScore(score: ScoreEntity) { scoreDao.insertScore(score) }
-    suspend fun clearScores() { scoreDao.clearAllScores() }
-}
