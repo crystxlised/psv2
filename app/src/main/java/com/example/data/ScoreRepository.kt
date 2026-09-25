f3c229f7143b51d0992e35d3c8a93a9c35151dd2
crystxlised <avkxdlol@gmail.com>
2026-09-25 16:18:41 +0530

    Add app/src/main/java/com/example/data/ScoreRepository.kt from PingleSpin

---
+++ app/src/main/java/com/example/data/ScoreRepository.kt
@@ -0,0 +1,9 @@
+package com.example.data
+
+import kotlinx.coroutines.flow.Flow
+
+class ScoreRepository(private val scoreDao: ScoreDao) {
+    val topScores: Flow<List<ScoreEntity>> = scoreDao.getTopScores()
+    suspend fun insertScore(score: ScoreEntity) { scoreDao.insertScore(score) }
+    suspend fun clearScores() { scoreDao.clearAllScores() }
+}
